8b2c33b4ec79fe339e7abb2eba14c19255551c06
severianowp <flavio.wp@katalid.com>
2020-10-12 17:35:50 -0300

    subindo manutencao

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div
+    id="app"
+    :style="backgroundGradient"
+    @mousemove="redefinePositions"
+    @mouseleave="resetPositions"
+  >
+    <img alt="Vue logo" src="./assets/logo.svg" />
+    <HelloWorld msg="Katalid em Manutenção" />
+  </div>
+</template>
+
+<script>
+import { bgColorFrom, bgColorTo } from "./styles/variables.scss";
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      gradientX: "50%",
+      gradientY: "0%",
+    };
+  },
+  computed: {
+    backgroundGradient() {
+      return {
+        background: `radial-gradient(circle at ${this.gradientX} ${this.gradientY}, ${bgColorFrom}, ${bgColorTo})`,
+      };
+    },
+  },
+  methods: {
+    redefinePositions({ clientX, clientY }) {
+      this.gradientX = `${clientX}px`;
+      this.gradientY = `${clientY}px`;
+    },
+    resetPositions() {
+      this.gradientX = "50%";
+      this.gradientY = "0";
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "./styles/reset.scss";
+body {
+  background-color: black;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  min-height: 100vh;
+
+  img {
+    width: 10vw;
+    display: flex;
+    flex-direction: column;
+    margin: 25vh auto 2vh;
+  }
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>Em breve, um site novo para você.</p>
+    <p>
+      Para maiores informações, envie um e-mail para <b>contato@katalid.com</b>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+p {
+  color: rgb(236, 226, 235);
+  line-height: 3rem;
+}
+</style>
